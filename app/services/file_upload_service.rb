32b976bbb2f6ad69ed3ebe46004be4c356486881
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-07-16 00:37:51 -0500

    File uploading service.

---
+++ app/services/file_upload_service.rb
@@ -0,0 +1,21 @@
+class FileUploadService
+  
+  def initialize(file_params)
+    @file = file_params[:file]
+  end
+
+  def create
+    message = {status: 'success', message: 'Uploaded successfully.'}
+    begin
+      rows = CSV.read(@file.path)
+    # convert headers
+      headers = rows.shift.map { | header| header.gsub!(/( )/, '_').downcase! }
+      # Generate contacts hash
+      contacts = rows.map { |row| Hash[headers.zip(row)] }
+      Contact.create(contacts)
+    rescue Exception => e
+      return { status: 'failed', message: 'Please uplaod file with valid data' }
+    end
+    message
+  end
+end
