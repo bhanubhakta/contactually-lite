008275a0e380d4add86a2ffb86e70206824e4ba7
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-07-16 00:38:12 -0500

    Add file upload logic.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,13 @@ class ContactsController < ApplicationController
   end
 
   def upload
-    
+    result = FileUploadService.new(upload_params).create
+    if result[:status] == 'failed'
+      flash[:error] = result[:message]
+      redirect_to root_path and return
+    end
+    flash[:success] = result[:message]
+    redirect_to contacts_path
   end
 
   def new
