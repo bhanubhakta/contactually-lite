725cb2f7451076c0f12f890d8ffc5e469fc819b6
Bhanu Bhakta Sigdel <bhanubhaktasigdel@Bhanus-MacBook-Pro.local>
2017-07-16 00:03:39 -0500

    Add route.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'contacts#new'
+  post 'contacts#upload'
+  resources :contacts, only: [:index, :create, :destroy]
 end
